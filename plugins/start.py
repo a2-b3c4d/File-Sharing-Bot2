e5047aa9cd08a6c7b9f8da086b0985876dc0b9b2
NTF-KURAMA <aveline.teamx@gmail.com>
2024-10-09 10:06:40 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -107,7 +107,9 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🔒 Close", callback_data = "close"),
+                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("😊 About Me", callback_data = "about")
                 ]
             ]
         )
